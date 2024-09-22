81f3280a2a22644e733a8e3bfea034b0e63f4e3a
Bence Toth <toth.bence.mihaly@gmail.com>
2024-09-22 14:23:11 +0200

    Create lambda_function.py

---
+++ lambda_function.py
@@ -0,0 +1,52 @@
+import os
+from datetime import datetime
+from urllib.request import Request, urlopen
+
+SITE = os.environ['site']  # URL of the site to check, stored in the site environment variable
+EXPECTED = os.environ['expected']  # String expected to be on the page, stored in the expected environment variable
+BUCKET_NAME = os.environ['bucket']
+
+def validate(file_as_string):
+    '''Return False to trigger the canary
+
+    Currently this simply checks whether the EXPECTED string is present.
+    However, you could modify this to perform any number of arbitrary
+    checks on the contents of SITE.
+    '''
+    import json
+    
+    obj = json.loads(file_as_string)
+
+    provider_name = obj['countries'][0]['name']
+    
+    return EXPECTED == provider_name
+    
+def write_to_bucket(contents: str, timestamp: str):
+    import boto3
+    file_name = f"{BUCKET_NAME}/{timestamp}.json"
+    
+    s3 = boto3.resource("s3")
+    s3.Bucket(BUCKET_NAME).put_object(Key=file_name, Body=contents)
+
+
+def lambda_handler(event, context):
+    print('Checking {} at {}...'.format(SITE, event['time']))
+
+    try:
+        req = Request(SITE, headers={'User-Agent': 'AWS Lambda'})
+        contents = str(urlopen(req).read().decode('utf-8'))
+
+        if not validate(contents):
+            raise Exception('Validation failed')
+
+        write_to_bucket(contents, event['time'])
+        
+    except:
+        print('Check failed!')
+        raise
+    else:
+        print('Check passed!')
+        return event['time']
+    finally:
+        print('Check complete at {}'.format(str(datetime.now())))
+
